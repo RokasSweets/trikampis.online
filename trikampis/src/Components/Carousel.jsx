2477bc6f2d9cf9433ee429302331d8f504e6a865
RokasSweets <sweetsound741@gmail.com>
2023-05-04 02:19:03 +0300

    carousel

--- trikampis/src/Components/Carousel.jsx
+++ trikampis/src/Components/Carousel.jsx
@@ -1,14 +1,38 @@
-import './Styles/Carousel.css';
-import picture from '../Images/carousel1.jpg'
+import React, { Component } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./Styles/Carousel.css";
 
-function Carousel() {
-  return (
-<div className='carousel col-10 mt-4 m-auto'>
-    <img src={picture} alt="carousel1" />
-    <h1>Sveiki atvykę!</h1>
-
-</div>
-  );
+export default class SimpleSlider extends Component {
+  render() {
+    const settings = {
+      dots: false,
+      fade: true,
+      infinite: false,
+      speed: 500,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+    };
+    return (
+      <div className="main-carousel col-12 col-lg-8 m-auto">
+        <Slider {...settings}>
+          <div className="first slide">
+            <h1 className="slide-title">Sveiki atvykę!</h1>
+            <p>
+              Sveikinu atradus slapčiausią konspiracijos ir sąmokslo teorijų
+              svetainę!
+            </p>
+          </div>
+          <div className="second slide">
+            <h1 className="slide-title">Ką čia galiu rasti?</h1>
+            <p>blablabla</p>
+          </div>
+        </Slider>
+      </div>
+    );
+  }
 }
-
-export default Carousel;
--- trikampis/src/Components/NavBar.jsx
+++ trikampis/src/Components/NavBar.jsx
@@ -3,15 +3,15 @@ import logo from '../Images/logo.png'
 
 function NavBar() {
   return (
-    <div className='navbar col-8 m-auto mt-2'>
+    <div className='navbar col-12 col-lg-8 m-auto mt-2'>
         <div className='logodiv col-2'>
 <img className='logoimg' src={logo} alt="logo" />
 <p className='logotext'>TRIKAMPIS.ONLINE</p>
         </div>
         <div className='links col-5'>
-<button>Home</button>
-<button>Categories</button>
-<button>About Us</button>
+<button>Pagrindinis</button>
+<button>Kategorijos</button>
+<button>Apie mus</button>
         </div>
 
     </div>
